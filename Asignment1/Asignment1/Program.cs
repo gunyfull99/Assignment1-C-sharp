7b258bdb735e350fc63e6d89eed39780eaf949c2
Nguyễn Minh Hiếu <hieu000502@gmail.com>
2022-05-17 15:12:59 +0700

    2

--- Asignment1/Asignment1/Program.cs
+++ Asignment1/Asignment1/Program.cs
@@ -10,63 +10,75 @@ public class Program
         List<double> list = new List<double>();
 
         
-        Regex rg = new Regex(@"^[n][a-zA-z]{2,}[^n]$");
+        Regex rg = new Regex(@"^[n|N][a-zA-z]{2,}[^n|^N]$");
         Regex rgNumber = new Regex(@"^-?\d+(\.?\d+)$");
         if (System.IO.Directory.Exists(sourcePath))
         {
             string[] files = System.IO.Directory.GetFiles(sourcePath);
             foreach (string s in files)
             {
-
                 Dictionary<string, int> dict = new Dictionary<string, int>();
                 List<char> listWord = new List<char>();
-
-
                 int countWord = 0;
-                //1
+                
                 String fileName = System.IO.Path.GetFileName(s);
-                if (fileName.Contains("việt nam"))
-                {
-                    String destFile = System.IO.Path.Combine(targetPath, fileName);
-                    System.IO.File.Copy(s, destFile, true);
-                }
-
+     
                 //2
                 StreamReader myfile;
-                myfile = File.OpenText(s);
+                try
+                {
+                    myfile = File.OpenText(s);
                 string line;
                 do
                 {
                     line = myfile.ReadLine();
-                    if (line != null)
-                        foreach (string str in line.Split(" "))
-                        {
-                                if (dict.ContainsKey(str))
+                        if (line != null)
+                            //  foreach (string str in line.Split(" "))
+                            // {
+
+                            //1
+                            for (int i = 0; i < line.Split(" ").Length; i++)
                                 {
-                                    dict[str] = dict[str] + 1;
+                                string word = line.Split(" ")[i].ToLower();
+
+                                if (word.Equals("việt") && line.Split(" ")[i + 1].ToLower().Equals("nam"))
+                                {
+                                    String destFile = System.IO.Path.Combine(targetPath, fileName);
+                                    System.IO.File.Copy(s, destFile, true);
+                                }
+
+                                if (dict.ContainsKey(word))
+                                {
+                                    dict[word] = dict[word] + 1;
                                 }
                                 else
                                 {
-                                    dict.Add(str, 1);
+                                    dict.Add(word, 1);
                                 }
-                                foreach (char word in str)
+                                foreach (char w in word)
                                 {
-                                    if (!listWord.Contains(word))
-                                    {
-                                        listWord.Add(word);
+                                    if (!listWord.Contains(w))
+                                    
+                                        listWord.Add(w);
                                     }
                                 }
-                        
-                        }
+                      //  }
                 }
                 while (line != null);
+                myfile.Close();
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine("wrong when open file.");
+                }
                 foreach (KeyValuePair<string, int> sortFile in dict.OrderByDescending(key => key.Value))
                 {
                     File.AppendAllText(s.Split(".txt")[0]+" 2"+".txt", sortFile.Key + " : " + sortFile.Value + "\n");
                     countWord += sortFile.Value;
                 //    Console.WriteLine(sortFile.Key + " : " + sortFile.Value);
                 }
-                myfile.Close();
+
+                //3
              File.AppendAllText(@"C:\Users\hieu0\OneDrive\Máy tính\c#\việt nam3.txt", fileName + " : "+ countWord + "\n");
 
                 //4
@@ -94,6 +106,8 @@ public class Program
                     File.AppendAllText(s.Split(".txt")[0] + " bai 6" + ".txt", listWord[i] + "\n");
                 }
             }
+
+            //5
             for (int i = 0; i < list.Count; i++)
             {
                 File.AppendAllText(@"C:\Users\hieu0\OneDrive\Máy tính\c#\Bai 5.txt", list[i] + "\n");
